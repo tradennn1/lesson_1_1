bc4b76b7df0e4fd0970fb8375ea739f930ee4ad4
tradennn1 <treidnnn@gmail.com>
2022-07-04 20:46:08 +0600

    Initial commit

---
+++ lesson_1_1/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(6 + 5);
+        System.out.println(10 - 4 / 2 * 5 + 1);
+        System.out.println(10 - 4 / 2 * (5 + 1));
+        System.out.println(7 % 3); // оператор модуль (modulus), дает остаток после деления
+        System.out.println("I am a " + "programmer");
+        System.out.println("Result: " + 6 * 3);
+        System.out.println("Result: " + 2 + 9);
+        System.out.println("Result: " + (2 + 9));
+        System.out.println((-1) + (-2));
+
+        int ageOfMyFriend = 21; // объявление и инициализация переменной )))
+
+        System.out.println("Возраст моего друга - " + ageOfMyFriend);
+        System.out.println(ageOfMyFriend + 10);
+        ageOfMyFriend = 25;
+        System.out.println(ageOfMyFriend);
+
+        int apples; // объявление переменной
+        System.out.println("Hi");
+        apples = 90 + 6; // инициализация переменной
+        System.out.println("Apples = " + apples);
+
+        char myFavoriteSymbol = 'm';
+        double digit = 89.223;
+        System.out.println(myFavoriteSymbol + " " + digit);
+
+        String word = "Car";
+        System.out.println(word);
+
+        final String myName = "Aleksey";
+        //myName = "Mirlan";
+    }
+}
